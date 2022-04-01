977e0bd5aaea39287c67b949c1101b62a5793205
Sanjay Idpuganti <sanjayidpuganti@outlook.com>
2022-04-01 23:50:46 +0530

    LoginForm and Tailwind related changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { ReactiveFormsModule } from '@angular/forms'
-import { DrinksIconComponent } from './components/icons/drinks-icon.component'
+import { DrinksIconComponent } from './components/icons/drinks-icon.component';
+import { LoginFormComponent } from './components/forms/login-form/login-form.component'
 
 @NgModule({
-  declarations: [AppComponent, DrinksIconComponent],
+  declarations: [AppComponent, DrinksIconComponent, LoginFormComponent],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   content: ['./src/**/*.{html,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      flexShrink: {
+        '2': 2
+      },
+      maxWidth: {
+        '1/2': "50%",
+        '3/4': "75%"
+      },
+      width: {
+        '1/2': "50%",
+        '4/6': "60%",
+        "2/6": "40%"
+      }
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/forms')],
 }
